6645b7e677e5011d0594b6c790caf58a71e85e03
Soňa Ouzká <ouzkasona@gmail.com>
2021-04-21 16:38:59 +0200

    Zmena výpisu kdo je na tahu + atribut disabled

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ console.log("funguju");
 
 let naTahu = 'circle';
 
-const hraButton = document.querySelector('.mrizka');
+const hraButtonElm = document.querySelector('.mrizka');
+const kdoHrajeElm = document.querySelector('.kdoHrajeImg');
 
-hraButton.addEventListener('click', (event) => {
+hraButtonElm.addEventListener('click', (event) => {
   if (naTahu === 'circle') {
     event.target.classList.add('mrizka--circle');
+    kdoHrajeElm.src = 'circle.svg';
+    event.target.disabled = true;
+    naTahu = 'cross';
+
   } else if (naTahu === 'cross') {
     event.target.classList.add('mrizka--cross');
+    kdoHrajeElm.src = 'cross.svg';
+    event.target.disabled = true;
+    naTahu = 'circle';
   }
 });
