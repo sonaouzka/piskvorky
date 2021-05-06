250d3bf2f946ae5a24980c709b5c9cd78ed249b1
Soňa Ouzká <ouzkasona@gmail.com>
2021-05-06 20:30:20 +0200

    Oprava - kdo je na tahu značení

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const kdoHrajeElm = document.querySelector('.kdoHrajeImg');
 hraButtonElm.addEventListener('click', (event) => {
   if (naTahu === 'circle') {
     event.target.classList.add('mrizka--circle');
-    kdoHrajeElm.src = 'circle.svg';
+    kdoHrajeElm.src = 'cross.svg';
     event.target.disabled = true;
     naTahu = 'cross';
 
   } else if (naTahu === 'cross') {
     event.target.classList.add('mrizka--cross');
-    kdoHrajeElm.src = 'cross.svg';
+    kdoHrajeElm.src = 'circle.svg';
     event.target.disabled = true;
     naTahu = 'circle';
   }
